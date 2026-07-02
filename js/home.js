173aa36190459541767c94901f06dfe4e203f448
MrinmoyBaust98 <baust.cse.170201073@gmail.com>
2026-07-02 11:23:53 +0600

    add money and cashout dull section complete

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+
+//  Add Money Functionalities
 document.getElementById('btn-add-money')
     .addEventListener('click', function (e) {
         e.preventDefault();
@@ -29,5 +31,44 @@ document.getElementById('btn-add-money')
         // show total update balance
         document.getElementById('total-blance').innerText = newTotalBlance;
 
-
     })
+// Cash Out Functionalities
+document.getElementById('btn-cash-out').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // validation
+    const agentAccount = document.getElementById('agent-account').value;
+    if (agentAccount.length !== 11) {
+        alert('Please Enter Correct Agent Account Number');
+        return;
+    }
+
+    const cashoutPin = document.getElementById('cashout-pin').value;
+    if (cashoutPin != 1234) {
+        alert('Please enter correct pin number');
+        return;
+    }
+    const cashOutAmount = parseInt(document.getElementById('cash-out-amount').value);
+    // Total blance access
+    const totalBlance = parseInt(document.getElementById('total-blance').innerText);
+
+    //  calculation for cashout
+    const newTotalBlance = totalBlance - cashOutAmount;
+
+    // display new amount
+    document.getElementById('total-blance').innerText = newTotalBlance;
+})
+
+
+// Toggle features
+
+document.getElementById('add-money-parent-btn').addEventListener('click', function () {
+    document.getElementById('cash-out-section').style.display = 'none';
+    document.getElementById('add-money-section').style.display = 'block';
+})
+
+document.getElementById('cash-out-parent-btn').addEventListener('click', function () {
+    document.getElementById('add-money-section').style.display = 'none';
+    document.getElementById('cash-out-section').style.display = 'block';
+
+})
